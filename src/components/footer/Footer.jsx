9601af3544d7ca4608e21edbb597353861988070
unknown <naman.aggarwal2001@gmail.com>
2020-12-29 18:53:52 +0530

    added foter styles hehe

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import google from "../../assets/google-play-badge.png";
 import "./Footer.css";
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <Grid container style={{ padding: "15px" }}>
+        <Grid
+          item
+          sm={6}
+          md={4}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <p>Contact Us</p>
+          <p className="small-link">Phone No :- +91-8697831037</p>
+          <p className="small-link">Email :- venatus@gmail.com</p>
+        </Grid>
+        <Grid
+          item
+          sm={12}
+          md={4}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={google}
+            alt="google-play"
+            style={{ width: "250px", height: "100px" }}
+          />
+        </Grid>
+        <Grid
+          item
+          sm={6}
+          md={4}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <p>Our Servers</p>
+          <p className="small-link">CSGO</p>
+          <p className="small-link">Minecraft</p>
+          <p className="small-link">Valorant</p>
+          <p className="small-link">CODM</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Footer;
