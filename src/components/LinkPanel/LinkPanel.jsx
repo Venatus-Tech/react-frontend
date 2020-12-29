7fb14522034b71b98167517e677ef88ed5ea8cb4
unknown <naman.aggarwal2001@gmail.com>
2020-12-29 17:42:57 +0530

    link-panel addded

--- src/components/LinkPanel/LinkPanel.jsx
+++ src/components/LinkPanel/LinkPanel.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { Paper, Grid } from "@material-ui/core";
+import fb from "../../assets/fb-icon.png";
+import insta from "../../assets/insta-icon.png";
+import discord from "../../assets/discord-icon.png";
+import youtube from "../../assets/utube-icon.png";
+import linkedin from "../../assets/linkedin-icon.png";
+import "./LinkPanel.css";
 const LinkPanel = () => {
-  return <div></div>;
+  return (
+    <div className="panel">
+      <div>
+        <img src={fb} alt="fb-icon" style={{ padding: "10px 15px" }} />
+      </div>
+      <div>
+        <img src={insta} alt="insta-icon" className="icons" />
+      </div>
+      <div>
+        <img src={discord} alt="discord-icon" className="icons" />
+      </div>
+      <div>
+        <img src={youtube} alt="utube-icon" className="icons" />
+      </div>
+      <div>
+        <img src={linkedin} alt="linkedin-icon" className="icons" />
+      </div>
+    </div>
+  );
 };
 
 export default LinkPanel;
--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Banner from "../../assets/ven-ban.png";
+import LinkPanel from "../../components/LinkPanel/LinkPanel";
 import "./Home.css";
+
 function Home() {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <img src={Banner} alt="Banner" className="banner" />
+
+      <LinkPanel />
     </div>
   );
 }
