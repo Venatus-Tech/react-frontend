bd5ccc83ddce2a5c71ae7941c3e59635c32a54f3
Pranav <pranavs036@gmail.com>
2020-12-29 12:04:22 +0530

    navbar update 2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Logo from "./venatusLogo.jpg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
+  const [clicked, onClicked] = useState(false);
+
   return (
     <div className='navbar'>
       <div className='v-logo-div'>
         <img src={Logo} alt='venatusLogo' className='vlogo' />
       </div>
-      <div className='list'>
+      <div className={clicked ? "list-active" : "list"}>
         <p className='list-item'>
           <Link to='/' style={{ color: "inherit", textDecoration: "inherit" }}>
             Home
@@ -58,7 +60,12 @@ function Navbar() {
       <div className='button'>
         <p className='register-text'>Register</p>
       </div>
-      <div className='icon-div'>
+      <div
+        className='icon-div'
+        onClick={() => {
+          onClicked(!clicked);
+        }}
+      >
         <FontAwesomeIcon icon={faBars} className='icon' />
       </div>
     </div>
