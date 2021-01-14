451c91e1d8a964d83b44b2e052e617bb6c44bbe7
unknown <naman.aggarwal2001@gmail.com>
2021-01-14 15:53:51 +0530

    form layout

--- src/screens/form/GeneralForm.js
+++ src/screens/form/GeneralForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Grid, makeStyles, TextField } from "@material-ui/core";
+import { Grid, makeStyles, TextField, Button } from "@material-ui/core";
 import firebase from "../../firebase";
 import * as Yup from "yup";
 import { Formik, ErrorMessage, Field, getIn, useFormik } from "formik";
@@ -153,130 +153,230 @@ const General = () => {
         }}
       >
         <h1>This is a form</h1>
-        <form className={classes.root} noValidate autoComplete="off">
-          <TextField
-            label="Email"
-            color="secondary"
-            helperText={formik.errors.email}
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            error={formik.touched.email && formik.errors.email ? true : false}
-            onBlur={formik.handleBlur}
-            name="email"
-            id="email"
-          />
-          <TextField
-            label="Name"
-            color="secondary"
-            helperText={formik.errors.name}
-            onChange={formik.handleChange}
-            value={formik.values.name}
-            error={formik.touched.name && formik.errors.name ? true : false}
-            onBlur={formik.handleBlur}
-            name="name"
-            id="name"
-          />
+        <Grid container>
+          <Grid item sm={4}>
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField
+                label="Email"
+                color="secondary"
+                helperText={formik.errors.email}
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                error={
+                  formik.touched.email && formik.errors.email ? true : false
+                }
+                onBlur={formik.handleBlur}
+                name="email"
+                id="email"
+              />
+              <TextField
+                label="Name"
+                color="secondary"
+                helperText={formik.errors.name}
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                error={formik.touched.name && formik.errors.name ? true : false}
+                onBlur={formik.handleBlur}
+                name="name"
+                id="name"
+              />
 
-          <TextField
-            helperText={formik.errors.phone}
-            onChange={formik.handleChange}
-            value={formik.values.phone}
-            error={formik.touched.phone && formik.errors.phone ? true : false}
-            onBlur={formik.handleBlur}
-            name="phone"
-            id="phone"
-            label="Phone No."
-            color="secondary"
-          />
+              <TextField
+                helperText={formik.errors.phone}
+                onChange={formik.handleChange}
+                value={formik.values.phone}
+                error={
+                  formik.touched.phone && formik.errors.phone ? true : false
+                }
+                onBlur={formik.handleBlur}
+                name="phone"
+                id="phone"
+                label="Phone No."
+                color="secondary"
+              />
 
-          <TextField
-            helperText={formik.errors.rollnum}
-            onChange={formik.handleChange}
-            value={formik.values.rollnum}
-            error={
-              formik.touched.rollnum && formik.errors.rollnum ? true : false
-            }
-            onBlur={formik.handleBlur}
-            name="rollnum"
-            id="rollnum"
-            label="Roll No."
-            color="secondary"
-          />
+              <TextField
+                helperText={formik.errors.rollnum}
+                onChange={formik.handleChange}
+                value={formik.values.rollnum}
+                error={
+                  formik.touched.rollnum && formik.errors.rollnum ? true : false
+                }
+                onBlur={formik.handleBlur}
+                name="rollnum"
+                id="rollnum"
+                label="Roll No."
+                color="secondary"
+              />
 
-          <TextField
-            helperText={formik.errors.branch}
-            onChange={formik.handleChange}
-            value={formik.values.branch}
-            error={formik.touched.branch && formik.errors.branch ? true : false}
-            onBlur={formik.handleBlur}
-            name="branch"
-            id="branch"
-            label="Branch"
-            color="secondary"
-          />
+              <TextField
+                helperText={formik.errors.branch}
+                onChange={formik.handleChange}
+                value={formik.values.branch}
+                error={
+                  formik.touched.branch && formik.errors.branch ? true : false
+                }
+                onBlur={formik.handleBlur}
+                name="branch"
+                id="branch"
+                label="Branch"
+                color="secondary"
+              />
 
-          <TextField
-            helperText={formik.errors.campus}
-            onChange={formik.handleChange}
-            value={formik.values.campus}
-            error={formik.touched.campus && formik.errors.campus ? true : false}
-            onBlur={formik.handleBlur}
-            name="campus"
-            id="campus"
-            label="Campus"
-            color="secondary"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={interest.isJuego}
-                onChange={handleChange}
-                name="isJuego"
+              <TextField
+                helperText={formik.errors.campus}
+                onChange={formik.handleChange}
+                value={formik.values.campus}
+                error={
+                  formik.touched.campus && formik.errors.campus ? true : false
+                }
+                onBlur={formik.handleBlur}
+                name="campus"
+                id="campus"
+                label="Campus"
+                color="secondary"
               />
-            }
-            label="Juego"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={interest.isAlfresco}
-                onChange={handleChange}
-                name="isAlfresco"
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={interest.isJuego}
+                    onChange={handleChange}
+                    name="isJuego"
+                  />
+                }
+                label="Juego"
               />
-            }
-            label="Alfresco"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={interest.isTech}
-                onChange={handleChange}
-                name="isTech"
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={interest.isAlfresco}
+                    onChange={handleChange}
+                    name="isAlfresco"
+                  />
+                }
+                label="Alfresco"
               />
-            }
-            label="Technical"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={interest.isProd}
-                onChange={handleChange}
-                name="isProd"
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={interest.isTech}
+                    onChange={handleChange}
+                    name="isTech"
+                  />
+                }
+                label="Technical"
               />
-            }
-            label="Production"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={interest.isPR}
-                onChange={handleChange}
-                name="isPR"
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={interest.isProd}
+                    onChange={handleChange}
+                    name="isProd"
+                  />
+                }
+                label="Production"
               />
-            }
-            label="Public Relations"
-          />
-        </form>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={interest.isPR}
+                    onChange={handleChange}
+                    name="isPR"
+                  />
+                }
+                label="Public Relations"
+              />
+            </form>
+          </Grid>
+          <Grid item sm={8} container>
+            <Grid item sm={1}>
+              <Button
+                style={{
+                  color: "yellow",
+                  background: "black",
+                  margin: "10px 0px",
+                }}
+              >
+                <span
+                  style={{
+                    writingMode: "vertical-rl",
+                    textOrientation: "upright",
+                  }}
+                >
+                  Juego
+                </span>{" "}
+              </Button>
+              <Button
+                style={{
+                  color: "yellow",
+                  background: "black",
+                  margin: "10px 0px",
+                }}
+              >
+                <span
+                  style={{
+                    writingMode: "vertical-rl",
+                    textOrientation: "upright",
+                  }}
+                >
+                  Alfresco
+                </span>{" "}
+              </Button>
+              <Button
+                style={{
+                  color: "yellow",
+                  background: "black",
+                  margin: "10px 0px",
+                }}
+              >
+                <span
+                  style={{
+                    writingMode: "vertical-rl",
+                    textOrientation: "upright",
+                  }}
+                >
+                  Tech
+                </span>{" "}
+              </Button>
+              <Button
+                style={{
+                  color: "yellow",
+                  background: "black",
+                  margin: "10px 0px",
+                }}
+              >
+                <span
+                  style={{
+                    writingMode: "vertical-rl",
+                    textOrientation: "upright",
+                  }}
+                >
+                  Prod.
+                </span>{" "}
+              </Button>
+              <Button
+                style={{
+                  color: "yellow",
+                  background: "black",
+                  margin: "10px 0px",
+                }}
+              >
+                <span
+                  style={{
+                    writingMode: "vertical-rl",
+                    textOrientation: "upright",
+                  }}
+                >
+                  PR
+                </span>{" "}
+              </Button>
+            </Grid>
+            <Grid item sm={11} style={{ padding: "0px 20px" }}>
+              <h1>Hello world</h1>
+            </Grid>
+          </Grid>
+        </Grid>
+
         <button
           style={{ width: "30%", height: "30px" }}
           onClick={formik.handleSubmit}
